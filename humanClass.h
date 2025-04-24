ce86f756a35b1df96b9948c2d33cd0f884314a31
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2025-04-24 19:12:35 +0300

    Add get methods to human class

--- humanClass.h
+++ humanClass.h
@@ -5,4 +5,9 @@ class Zmogus {
     private:
         string vardas_;
         string pavarde_;
+    public:
+        //get'eriai
+        inline std::string vardas() const { return vardas_; }    
+        inline std::string pavarde() const { return pavarde_; } 
+
 };

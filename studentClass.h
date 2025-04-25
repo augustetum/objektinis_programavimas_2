804cde1ef5cdad07d6a2198b52e4c1b1d45cf4af
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2025-04-25 21:29:09 +0300

    Adapt studentas class to being derived

--- studentClass.h
+++ studentClass.h
@@ -27,8 +27,6 @@ using std::istringstream;
 
 class Studentas : public Zmogus {
     private:
-        string vardas_;
-        string pavarde_;
         int egzaminas_;
         double galutinisMed_;
         double galutinisVid_;
@@ -37,46 +35,40 @@ class Studentas : public Zmogus {
     
     public:
         Studentas(string var, string pav, vector<int> paz, int egz) :  
-        vardas_{var}, pavarde_{pav}, pazymiai_{paz}, egzaminas_{egz} {
+        Zmogus(var, pav), pazymiai_{paz}, egzaminas_{egz} {
             skaiciuotiGalutiniSuMed();
             skaiciuotiGalutiniSuVid();
         }
 
-        Studentas(string var, string pav) : vardas_{var}, pavarde_{pav}, egzaminas_(0) {
+        Studentas(string var, string pav) : Zmogus(var, pav), egzaminas_(0) {
         }
-        Studentas() : egzaminas_(0) {} 
+        Studentas() : Zmogus(), egzaminas_(0) {} 
         Studentas(std::istream& is);
 
         //copy konstruktorius
         Studentas(const Studentas &s)
-        : vardas_{s.vardas_}, pavarde_{s.pavarde_}, egzaminas_{s.egzaminas_}, 
+        : Zmogus(s.vardas(), s.pavarde()), egzaminas_{s.egzaminas_}, 
         galutinisMed_{s.galutinisMed_}, galutinisVid_{s.galutinisVid_}, pazymiuVidurkis_{s.pazymiuVidurkis_},
         pazymiai_{s.pazymiai_}{}
 
         //move konstruktorius
         Studentas(Studentas&& s)
-        : vardas_{std::move(s.vardas_)}, pavarde_{std::move(s.pavarde_)}, egzaminas_{s.egzaminas_}, 
+        : Zmogus(s.vardas(), s.pavarde()), egzaminas_{s.egzaminas_}, 
         galutinisMed_{s.galutinisMed_}, galutinisVid_{s.galutinisVid_}, pazymiuVidurkis_{s.pazymiuVidurkis_},
         pazymiai_{std::move(s.pazymiai_)}{}
 
         //destruktorius
         ~Studentas(){
-            vardas_.clear();
-            pavarde_.clear();
             pazymiai_.clear();
         };
 
         //get'eriai
-        inline std::string vardas() const { return vardas_; }    
-        inline std::string pavarde() const { return pavarde_; } 
         inline double galutinisMed() const { return galutinisMed_;}
         inline double galutinisVid() const { return galutinisVid_;}
         inline const vector<int>& pazymiai() const { return pazymiai_; }
         inline int egzaminas() const { return egzaminas_; }
 
         //set'eriai
-        inline void setVardas(string var) { vardas_ = var; }
-        inline void setPavarde(string pav) { pavarde_ = pav; }
         inline void setGalutinisV(double galutVid) { galutinisVid_ = galutVid; }
         inline void setGalutinisM(double galutMed) { galutinisMed_ = galutMed; }
         inline void setEgzaminas(int egz) {egzaminas_ = egz; }
@@ -90,8 +82,8 @@ class Studentas : public Zmogus {
         Studentas& operator=(const Studentas& s){
             if (this == &s) return *this;
 
-            vardas_ = s.vardas_;
-            pavarde_ = s.pavarde_;
+            setVardas(s.vardas());
+            setPavarde(s.pavarde());
             egzaminas_ = s.egzaminas_;
             galutinisMed_ = s.galutinisMed_;
             galutinisVid_ = s.galutinisVid_;
@@ -105,21 +97,24 @@ class Studentas : public Zmogus {
         Studentas& operator =(Studentas&& s){
             if (this == &s) return *this;
 
-            vardas_ = std::move(s.vardas_);
-            pavarde_ = std::move(s.pavarde_);
+            setVardas(s.vardas());
+            setPavarde(s.pavarde());
             egzaminas_ = s.egzaminas_;
             galutinisMed_ = s.galutinisMed_;
             galutinisVid_ = s.galutinisVid_;
             pazymiuVidurkis_ = s.pazymiuVidurkis_;
             pazymiai_ = std::move(s.pazymiai_);
+
+            s.setVardas("");
+            s.setPavarde("");
             
             return *this;
         }
 
-        //output operator
+        //output operatorius
         friend std::ostream& operator<<(std::ostream& out, const Studentas &s);
 
-        //input operator
+        //input operatorius
         friend std::istream& operator>>(std::istream& cin, Studentas& s);
 };
 

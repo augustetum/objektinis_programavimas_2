6d5ff540f038caf334c719880c92c52190934d63
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2025-05-07 15:57:54 +0300

    Add Doxygen files

---
+++ html/files_dup.js
@@ -0,0 +1,6 @@
+var files_dup =
+[
+    [ "humanClass.h", "human_class_8h_source.html", null ],
+    [ "studentClass.h", "student_class_8h_source.html", null ],
+    [ "timer.h", "timer_8h_source.html", null ]
+];
---
+++ html/navtreeindex0.js
@@ -0,0 +1,16 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[1,0],
+"class_studentas.html":[1,0,0],
+"class_timer.html":[1,0,1],
+"class_zmogus.html":[1,0,2],
+"classes.html":[1,1],
+"files.html":[2,0],
+"hierarchy.html":[1,2],
+"human_class_8h_source.html":[2,0,0],
+"index.html":[],
+"md__r_e_a_d_m_e.html":[0],
+"pages.html":[],
+"student_class_8h_source.html":[2,0,1],
+"timer_8h_source.html":[2,0,2]
+};
